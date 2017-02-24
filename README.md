da012e16879f655c8c381f03882b5c775053dfb4
Athul <athul.satheesh166@gmail.com>
2017-02-24 08:07:06 +0000

    fifth

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 athul
 new line added
 next line added
+add
--- mergeDecision.sh
+++ mergeDecision.sh
@@ -2,7 +2,6 @@
 error=0
 warning=0
 if [ $error==0 ] && [ $warning==0 ] ; then
-	git merge master
 	git checkout master
 	git merge develop
 	git push origin master
